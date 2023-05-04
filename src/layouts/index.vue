9f6975fa703b8099ad57fc4b09ce4faefd2e44df
masicheng <297876926@qq.com>
2023-05-04 18:45:47 +0800

    feat(layout): update layout

---
+++ src/layouts/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <a-layout>
+      <a-layout-sider>
+        <LayoutSide></LayoutSide>
+      </a-layout-sider>
+      <a-layout>
+        <a-layout-header>Header</a-layout-header>
+        <a-layout-content>Content</a-layout-content>
+        <a-layout-footer>Footer</a-layout-footer>
+      </a-layout>
+    </a-layout>
+  </div>
+</template>
+
+<script setup>
+import LayoutHeader from "./components/LayoutHeader.vue"
+import LayoutSide from "./components/LayoutSide.vue"
+</script>
+
+<style scoped></style>
