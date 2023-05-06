9985bf79d683b687845a13f68bec7d59ebb0d3c7
masicheng <297876926@qq.com>
2023-05-06 18:37:21 +0800

    feat(build): add build output chunks

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,21 @@ import vue from "@vitejs/plugin-vue"
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port:3000
+    port: 3000,
   },
   plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          system: ["./src/view/system/login"],
+        },
+      },
     },
   },
 })
