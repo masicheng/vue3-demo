d7f7777cc00bd3427338ad2e113e7651628fd757
masicheng <297876926@qq.com>
2023-05-10 17:21:21 +0800

    fix(util): update storage

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,26 +1,22 @@
 class StorageUtil {
-    constructor(storage) {
-        this.storage = storage
-    }
-    setItem(key, value) {
-        this.storage.setItem(key, JSON.stringify(value))
-    }
-    getItem(key) {
-        const value = this.storage.getItem(key)
-        value ?
-            return JSON.parse(value): null
-    }
-    removeItem(key) {
-        this.storage.removeItem(key)
-    }
+  constructor(storage) {
+    this.storage = storage
+  }
+  setItem(key, value) {
+    this.storage.setItem(key, JSON.stringify(value))
+  }
+  getItem(key) {
+    const value = this.storage.getItem(key)
+    return value ? JSON.parse(value) : null
+  }
+  removeItem(key) {
+    this.storage.removeItem(key)
+  }
 
-    clear() {
-        this.storage.clear()
-    }
+  clear() {
+    this.storage.clear()
+  }
 }
-const localStorage = new StorageUtil(localStorage)
-const sessionStorage = new StorageUtil(sessionStorage)
-module.exports = {
-    localStorage: ,
-    sessionStorage
-}
+const localStorageUtil = new StorageUtil(localStorage)
+const sessionStorageUtil = new StorageUtil(sessionStorage)
+export { localStorageUtil, sessionStorageUtil }
