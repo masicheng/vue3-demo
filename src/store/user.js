758d8d379b0f3938ff04dcd7c24b700228689048
masicheng <297876926@qq.com>
2023-05-12 18:53:48 +0800

    fix(store): update store

--- src/store/user.js
+++ src/store/user.js
@@ -2,20 +2,33 @@ import { defineStore } from "pinia"
 import { ref } from "vue"
 import { localStorageUtil } from "@/utils/storage"
 import  router  from "@/router"
-
+import { Modal } from "ant-design-vue"
+import api from '@/api'
 
 export const useUserStore = defineStore("user", () => {
   const userInfo = ref({})
-  function login(userInfo) {
+  async function  login(param) {
+    const {goHome=true,mode="",}
     this.userInfo = userInfo
-    localStorageUtil.setItem("userInfo", this.userInfo)
+    const data =await api.login(loginParam)
+    this.setToken(token)
+  }
+  function setToken(token){
+    localStorageUtil.setItem('token',)
+  }
+  function getToken(){
+    return localStorageUtil.getItem('token')
   }
   function logout(goLogin = false) {
     this.userInfo = {}
     localStorageUtil.clear()
     goLogin && router.push("Login")
   }
-  function confirmLoginout() {
+//   function after
+  async function getUserInfoAsync(token){
+
+  }
+  function confirmLoginout(errMsg) {
     Modal.confirm({
       title: errMsg,
       content: "点击确认前往授权登录",
@@ -24,4 +37,5 @@ export const useUserStore = defineStore("user", () => {
       },
     })
   }
+  return {userInfo,login,logout,confirmLoginout}
 })
