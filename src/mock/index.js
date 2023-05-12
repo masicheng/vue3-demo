58248c3f16c32e7914d0bc8d2f190147b4d9d894
masicheng <297876926@qq.com>
2023-05-12 18:53:18 +0800

    feat(mock): add user mock

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,9 +1,26 @@
-import Mock from "mockjs";
+import Mock from "mockjs"
+const { mock, Random } = Mock
 
-Mock.mock('/login','post',{
-    status:200,
-    data:{
-        name:"马四成",
-        token:'123'
+mock("/login", "post", (options) => {
+  const { username, password } = JSON.parse(options.body)
+  console.log(username, password)
+  if (username === "admin" && password === "123456") {
+    return {
+      code: 200,
+      data: {
+        token: Random.id(),
+      },
     }
+  } else {
+    return {
+      code: 400,
+      errMsg: "用户名或密码有误，请重新输入",
+    }
+  }
+})
+mock("/getUserInfo", "get", (options) => {
+  return {
+    name: Random.name(),
+    avatar: Random.dataImage("100*100"),
+  }
 })
