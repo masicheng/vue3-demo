67ceec817bd9e7ae12dc46a4b23c186cadf7f200
masicheng <297876926@qq.com>
2023-05-17 11:10:08 +0800

    fix(app): update main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 import { createApp } from "vue"
-import { createPinia } from "pinia"
 
 import App from "./App.vue"
-import router from "./router"
+import {setupRouter} from "./router"
 import Antd from "ant-design-vue"
-import './mock/index'
+import "./mock/index"
 import "ant-design-vue/dist/antd.css"
-import 'virtual:uno.css'
-import '@unocss/reset/normalize.css'
-import '@/assets/css/main.less'
+import "virtual:uno.css"
+import "@unocss/reset/normalize.css"
+import "@/assets/css/main.less"
+import { setupStore } from "./store"
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+setupStore(app)
+setupRouter(app)
 app.use(Antd)
 
 app.mount("#app")
