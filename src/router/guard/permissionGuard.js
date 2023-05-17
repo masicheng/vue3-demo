68be13348523870c349a82c1d77984ed6c7cb490
masicheng <297876926@qq.com>
2023-05-17 11:09:28 +0800

    fix(router): update router module

--- src/router/guard/permissionGuard.js
+++ src/router/guard/permissionGuard.js
@@ -1,7 +1,15 @@
+import { useUserStoreWithout } from "@/store/modules/user"
+import { loginPath, homePath } from "@/setting/projectSetting"
+
 export function createPermissionGuard(router) {
+  const userStore = useUserStoreWithout()
   router.beforeEach((to, from) => {
-    if (to.name !== "Login") {
-      return { name: "Login" }
+    const token = userStore.getToken()
+    if (!token && to.path !== loginPath) {
+      return { path: loginPath }
+    }
+    if (token && to.path === loginPath) {
+      return { path: homePath }
     }
   })
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router"
 import { setupRouterGuard } from "./guard"
-const router = createRouter({
+export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/login",
-      name: "Login",
+      name: "login",
       component: () => import("@/views/system/login/Login.vue"),
     },
     {
@@ -23,6 +23,9 @@ const router = createRouter({
     },
   ],
 })
+export function setupRouter(app){
+    setupRouterGuard(router)
+    app.use(router)
+}
 
-setupRouterGuard(router)
-export default router
+
