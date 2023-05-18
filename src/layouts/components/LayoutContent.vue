ef5b1d905899e8abff34ea91bdca219fc851bb86
masicheng <297876926@qq.com>
2023-05-18 18:47:24 +0800

    fix(layouts): update layouts

---
+++ src/layouts/components/LayoutContent.vue
@@ -0,0 +1,21 @@
+<template>
+  <RouterView>
+    <template #default="{ Component, route }">
+      <transition
+        mode="out-in"
+        appear
+      >
+        <keep-alive v-if="openCache" :include="getCaches">
+          <component :is="Component" :key="route.fullPath" />
+        </keep-alive>
+        <component v-else :is="Component" :key="route.fullPath" />
+      </transition>
+    </template>
+  </RouterView>
+</template>
+
+<script setup>
+    
+</script>
+
+<style scoped></style>
--- src/layouts/components/LayoutSide.vue
+++ src/layouts/components/LayoutSide.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        
+        side
     </div>
 </template>
 
