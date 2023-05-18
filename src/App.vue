682f3e07fb78ba830f864bf589f45ffa9e1a289f
masicheng <297876926@qq.com>
2023-05-18 18:48:51 +0800

    fix(views): update function

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue"
+  import HelloWorld from './components/HelloWorld.vue';
+  import Layout from './layouts/index.vue';
 </script>
 
 <template>
   <a-config-provider>
-    <RouterView />
+    <Layout>
+      <RouterView />
+    </Layout>
   </a-config-provider>
 </template>
 
 <style scoped>
-
 </style>
--- src/views/system/login/Login.vue
+++ src/views/system/login/Login.vue
@@ -7,46 +7,39 @@
       class="w-400px"
       @submit="loginSubmit"
     >
-      <a-form-item
-        label="用户名"
-        name="username"
-      >
+      <a-form-item label="用户名" name="username">
         <a-input v-model:value="formData.username"></a-input>
       </a-form-item>
-      <a-form-item
-        label="密码"
-        name="password"
-      >
+      <a-form-item label="密码" name="password">
         <a-input-password v-model:value="formData.password"></a-input-password>
       </a-form-item>
       <a-form-item :wrapper-col="{ offset: 8, span: 8 }">
-        <a-button
-          class="w-full"
-          type="primary"
-          html-type="submit"
-          >登录</a-button
-        >
+        <a-button class="w-full" type="primary" html-type="submit">登录</a-button>
       </a-form-item>
     </a-form>
   </div>
 </template>
 
 <script setup>
-import api from "@/api"
-import { reactive } from "vue"
-import { useUserStore } from "@/store/modules/user"
-import { notification } from "ant-design-vue"
+  import api from '@/api';
+  import { reactive } from 'vue';
+  import { useUserStore } from '@/store/modules/user';
+  import { notification } from 'ant-design-vue';
 
-const userStore = useUserStore()
+  const userStore = useUserStore();
 
-const formData = reactive({
-  username: "admin",
-  password: "123456",
-})
-async function loginSubmit(e) {
-  await userStore.login(formData)
-  notification.success({ message: "登录成功！" })
-}
+  const formData = reactive({
+    username: 'admin',
+    password: '123456',
+  });
+  async function loginSubmit(e) {
+    const userInfo = await userStore.login(formData);
+    notification.success({
+      message: '登录成功！',
+      description: `欢迎您，${userInfo.name}`,
+      duration: 3,
+    });
+  }
 </script>
 
 <style lang="scss" scoped></style>
